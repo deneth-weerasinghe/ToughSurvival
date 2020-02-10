79664d41336586d79be0055eeace8e6c3e36e97f
Deneth Weerasinghe <deneth@hotmail.co.uk>
2020-02-10 02:04:49 +0000

    moved onItemUse to HydrationEvents.java

--- src/main/java/com/denethweerasinghe/toughsurvival/handlers/HydrationEvents.java
+++ src/main/java/com/denethweerasinghe/toughsurvival/handlers/HydrationEvents.java
@@ -1,16 +1,22 @@
 package com.denethweerasinghe.toughsurvival.handlers;
 
+import com.denethweerasinghe.toughsurvival.itemdata.IItemHydration;
+import com.denethweerasinghe.toughsurvival.itemdata.ItemHydration;
 import com.denethweerasinghe.toughsurvival.playerdata.hydrationdata.Hydration;
 import com.denethweerasinghe.toughsurvival.playerdata.hydrationdata.IHydration;
 import com.denethweerasinghe.toughsurvival.setup.ToughSurvival;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -95,6 +101,56 @@ public class HydrationEvents {
             cap.setDecayFactor(cap.getDecayFactor() - 2.5F);
         }
     }
+
+    @SubscribeEvent
+    public static void onItemUse(LivingEntityUseItemEvent.Finish event){
+
+        Entity entity = event.getEntity();
+
+        if (entity instanceof PlayerEntity && !entity.getEntityWorld().isRemote) {
+
+            ItemStack itemStack = event.getItem();
+            Item item = itemStack.getItem();
+            IItemHydration cap = ItemHydration.getFromItem(itemStack);
+
+            // checks which items will be assigned a value
+            if (item == Items.APPLE){
+                cap.setHydration(2);
+            }
+            else if (item == Items.MELON_SLICE){
+                cap.setHydration(3);
+            }
+            else if (item == Items.CARROT){
+                cap.setHydration(2);
+            }
+            else if (item == Items.SWEET_BERRIES){
+                cap.setHydration(2);
+            }
+            else if (item == Items.BEETROOT){
+                cap.setHydration(3);
+            }
+            else if (item == Items.POTION){
+                cap.setHydration(4);
+                // water bottles are just special potions hence I have to use NBT tags
+                // to get the potion that corresponds to water bottles
+                if (itemStack.getTag().getString("Potion").equals("minecraft:water")){
+                    cap.setHydration(10);
+                }
+            }
+            else if (item == Items.WATER_BUCKET){
+                cap.setHydration(10);
+            }
+            else if (item == Items.MILK_BUCKET){
+                cap.setHydration(6);
+            }
+
+            // adding to playerHydration
+            IHydration playerCap = Hydration.getFromPlayer((PlayerEntity) entity);
+            playerCap.setHydration(playerCap.getHydration() + cap.getHydration());
+            Hydration.updateClient((ServerPlayerEntity) entity, playerCap);
+        }
+    }
+
 
     private static void applyEffects(PlayerEntity player, int hydration){
 
